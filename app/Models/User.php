b7bda397bbcd180c126212da421492463d566ea2
chhayhakk <chhayhakkourk@gmail.com>
2024-05-15 12:35:52 +0700

    Your commit message

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
+        'profile',
+        'status',
+        'is_admin',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\IsAdmin;
 Route::get('/', function () {
     return view('auth.register');
 });
-Route::get('/index', function () {
-    return view('auth.index');
+// Route::get('/index', function () {
+//     return view('auth.main');
+// });
+
+Route::middleware(['auth','verified'])->group(function(){
+    Route::get('/index', function(){
+        return view('auth.index');
+    });
 });
 
+// Route::middleware(['auth'.'verified', IsAdmin::class])->group(function(){
+//     Route::get('/index', function(){
+//         return view('auth.index');
+//     });
+// });
 
-Route::get('/profile', function(){
-    return "Welcome....";
-})->middleware(['auth', 'verified']);
 
 
