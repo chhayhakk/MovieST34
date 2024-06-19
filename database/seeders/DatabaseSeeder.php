3c489955a106cd2e8a16a3fc8915ae8e95a27e27
chhayhakk <chhayhakkourk@gmail.com>
2024-06-19 21:06:51 +0700

    Add Review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Movies;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Movies::factory()->create([
+        'title' => 'Duty After School',
+        'description' => 'This is a Tv Show',
+        'duration' => '2:22:22',
+        'year' => '2022',
+        'image_path' => 'dutyafterschool.jpg',
+        'created_at' => Carbon::now(),
+        'updated_at'=>Carbon::now(),
+        'release' => '2022',
+        'quality' => '4k',
+        'type' => 'tvshow',
+        'back_photo' => 'dutyafterschoolback.jpeg',
+        'user_id'=>'1',
+        'genre' =>'1',
         ]);
     }
+
 }
